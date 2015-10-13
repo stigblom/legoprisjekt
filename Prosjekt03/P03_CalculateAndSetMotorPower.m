30f4f197d6a10e879237d8fc28f644a5100beca6
Oliver <ol.salvesen@gmail.com>
2015-10-13 19:45:40 +0200

    Fjernet kommentering /rot

--- Prosjekt03/P03_CalculateAndSetMotorPower.m
+++ Prosjekt03/P03_CalculateAndSetMotorPower.m
@@ -7,9 +7,7 @@ if online
     elseif PID(k) < -(100-fart)
         PID(k) = -(100-fart)
     end
-    % Beregner motorpådrag og lagrer i datavektor
-    %PowerA(k) = JoyForover(k)+JoySide(k);
-    %PowerC(k) = JoyForover(k)-JoySide(k);
+
     motorA.Power = fart - PID(k);
     motorA.SendToNXT();
     motorC.Power = fart + PID(k);
@@ -17,42 +15,6 @@ if online
     PowerA(k) = motorA.Power;
     PowerC(k) = motorC.Power;
    
-%     if LysDerivert(k) < 3 && LysDerivert(k) > -3
-%         motorA.Power = 25;
-%         motorA.SendToNXT();
-%         motorC.Power = 25;
-%         motorC.SendToNXT();
-%         PowerA(k) = motorA.Power;
-%         PowerC(k) = motorC.Power;
-%     elseif LysDerivert(k) < -3 && LysDerivert(k) > -15
-%         motorA.Power = 25;
-%         motorA.SendToNXT();
-%         motorC.Power = 12;
-%         motorC.SendToNXT();
-%         PowerA(k) = motorA.Power;
-%         PowerC(k) = motorC.Power;        
-%     elseif LysDerivert(k) > 3 && LysDerivert(k) < 15
-%         motorA.Power = 12;
-%         motorA.SendToNXT();
-%         motorC.Power = 25;
-%         motorC.SendToNXT();
-%         PowerA(k) = motorA.Power;
-%         PowerC(k) = motorC.Power; 
-%     elseif LysDerivert(k) < -15
-%         motorA.Power = 25;
-%         motorA.SendToNXT();
-%         motorC.Power = 0;
-%         motorC.SendToNXT();
-%         PowerA(k) = motorA.Power;
-%         PowerC(k) = motorC.Power;
-%     else
-%         motorA.Power = 0;
-%         motorA.SendToNXT();
-%         motorC.Power = 25;
-%         motorC.SendToNXT();
-%         PowerA(k) = motorA.Power;
-%         PowerC(k) = motorC.Power;
-%     end
 
 else
     pause(0.01) % simulerer NXT-Matlab kommunikasjon i online=0
